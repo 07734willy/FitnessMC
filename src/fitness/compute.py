7030e10672ffb68d6843a482a4ddaa72188686aa
Dillon Davis <07734willy@users.noreply.github.com>
2021-06-16 04:08:34 -0400

    added more customization to xp system

--- src/fitness/compute.py
+++ src/fitness/compute.py
@@ -1,6 +1,12 @@
 from .minecraft import set_mcmmo_skill
 import json
 
+with open("activities.json", "r") as f:
+	ACTIVITY_TABLE = json.load(f)
+
+with open("skills.json", "r") as f:
+	SKILL_TABLE = json.load(f)
+
 def decay_by_x_agg(x):
 	def agg(values):
 		values = sorted(values, reverse=True)
@@ -12,42 +18,53 @@ def apply_x_rate_decay(df, x):
 	new_df = df.groupby(['Date']).agg(decay_by_x_agg(x))
 	return new_df
 
-def apply_x_day_bonus(df, x):
+def apply_x_day_y_bonus(df, x, y):
 	sel = df['score'] > 0
 	nz_df = df[sel]
 
 	score_csum = nz_df['score'].cumsum()
 	nz_df.loc[:, 'score_avg'] = (score_csum - score_csum.shift(x, fill_value=0)) / x
-	df.loc[sel, 'score'] *= (nz_df['score'] / nz_df['score_avg']).clip(1.0, 1.1)
+	df.loc[sel, 'score'] *= (nz_df['score'] / nz_df['score_avg']).clip(1.0, 1.0 + y)
 
-def calculate_pushup(df):
-	df = df.copy()
+def calculator_wrapper(func):
+	def wrapper(name, df):
+		df = func(df.copy())
+		df = df[['Date', 'score']]
+		
+		activity_data = ACTIVITY_TABLE[name]
+
+		decay_rate = activity_data['decay_rate']
+		df = apply_x_rate_decay(df, decay_rate)
+		apply_x_day_y_bonus(df, 7, 0.15)
+
+		multiplier = activity_data['multiplier']
+		return df['score'].sum() * multiplier
+	return wrapper
+
+@calculator_wrapper
+def calculate_reps_only(df):
 	df.loc[:, 'score'] = df['Reps']
-	df = df[['Date', 'score']]
+	return df
 
-	df = apply_x_rate_decay(df, 0.7)
-	apply_x_day_bonus(df, 3)
-
-	return df['score'].sum()
+@calculator_wrapper
+def calculate_reps_times_weight(df):
+	df.loc[:, 'score'] = df['Reps'] * df['Weight']
+	return df
 
 CALCULATOR_TABLE = {
-	"Pushup": calculate_pushup,
+	"Pushup": calculate_reps_only,
 }
-
-with open("skills.json", "r") as f:
-	SKILL_TABLE = json.load(f)
 
 def calculate_activity_xp(workout_log):
 	activity_xp = {}
 	for activity_name, func in CALCULATOR_TABLE.items():
 		df = workout_log[workout_log['Exercise'] == activity_name]
-		activity_xp[activity_name] = func(df)
+		activity_xp[activity_name] = func(activity_name, df)
 	return activity_xp
 
 def calculate_skills_xp(activity_xp, player):
-	return {skill_data['name']: int(sum(activity_xp[activity_name] * rate 
-			for activity_name, rate in skill_data['rates'].items()
+	return {skill_data['name']: int(sum(activity_xp[activity_name] * skill_data['multiplier'] * rate / sum(skill_data['distribution'].values())
+			for activity_name, rate in skill_data['distribution'].items()
 		) + skill_data['bonuses'].get(player.minecraft_id, 0))
 		for skill_data in SKILL_TABLE}
-		# set_mcmmo_skill(player, skill_data['name'], xp)
 
